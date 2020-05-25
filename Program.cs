fad19a2b4250796c3bc0e3c31f4bfcdd79304552
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2020-05-26 00:14:17 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicrosoftGraphToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
